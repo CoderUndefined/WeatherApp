e4c90a126acd956dbd3202cb3edca9f08edfbdda
huukim@gmx.net <huukim@gmx.net>
2021-05-19 17:28:33 +0200

    Initial commmit

---
+++ app/src/main/java/com/example/weatherapp/Global.java
@@ -0,0 +1,12 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Some global values.
+ */
+public class Global {
+    public static AppCompatActivity a;
+    public static String city;
+    public static boolean first = true;
+}
